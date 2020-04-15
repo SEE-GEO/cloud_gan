76628d90a1f40fbce0387d64cf6d28827e8b7837
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-04-15 19:11:57 +0200

    Added infor on how to install wxdata.

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 This project investigates the potential of using generative adversarial networks
 (GANs) to generate cloud fields.
+
+## Dependencies
+
+I put together some of the code that I had to read data in a package called
+`wxdata` available from my github account. To install it run
+
+```
+git clone https://github.com/simonpf/wxdata
+cd wxdata
+pip install -e .
+```
--- examples/read_cloudsat_data.py
+++ examples/read_cloudsat_data.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
+
 from wxdata import Index
 
 #
@@ -12,10 +14,10 @@ dendrite_path = "~/Dendrite"
 index = Index.load(os.path.join(dendrite_path,
                                 "SatData/CloudSat/2b_geoprof.index"))
 
-start = datetime(2015, 01, 01)
-end   = datetime(201666666 01)
+start = datetime(2015, 1, 1)
+end   = datetime(2016, 1, 1)
 
-files = index.get_files("CloudSat_2b_geoprof",
+files = index.get_files("CloudSat_2b_GeoProf",
                         start = start,
                         end = end)
 
@@ -30,11 +32,16 @@ file = files[ind].open()
 
 z = file.altitude
 x = file.latitude
-x = np.broadcast_to(x.reshape(1, -1), z.shape)
+x = np.broadcast_to(x.reshape(-1, 1), z.shape)
 rr = file.radar_reflectivity
 
+from matplotlib.colors import Normalize
 f,ax = plt.subplots(1, 1)
-ax.pcolormesh(x, z, file.radar_reflectivity)
+norm = Normalize(-30, 30)
+ax.pcolormesh(x, z / 1e3, file.radar_reflectivity, norm=norm)
 ax.set_xlabel("Latitude [$^\circ\ N$]")
+ax.set_xlim([-30, 30])
 ax.set_ylabel("Altitude [km]")
 ax.set_ylim([0, 20])
+
+plt.show()
