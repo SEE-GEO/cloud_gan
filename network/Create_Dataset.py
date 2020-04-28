91e3fe5ba2751d3f9167b06f4cba2dce32a4485a
carlssss <svcarl@student.chalmers.se>
2020-04-28 13:14:05 +0200

    added so that it is possible to use cloudsat images as input to discriminator network

--- network/Create_Dataset.py
+++ network/Create_Dataset.py
@@ -5,49 +5,50 @@ import numpy.ma as ma
 from datetime import datetime
 
 from wxdata import Index
+def create_dataset():
+    dendrite_path = "~/Dendrite"
+    index = Index.load(os.path.join(dendrite_path,
+                                    "SatData/CloudSat/2b_geoprof.index"))
 
-dendrite_path = "~/Dendrite"
-index = Index.load(os.path.join(dendrite_path,
-                                "SatData/CloudSat/2b_geoprof.index"))
+    start = datetime(2015, 1, 1)
+    end   = datetime(2016, 1, 1)
 
-start = datetime(2015, 1, 1)
-end   = datetime(2016, 1, 1)
+    files = index.get_files("CloudSat_2b_GeoProf",
+                            start = start,
+                            end = end)
+    ind=0;
+    file = files[ind].open()
 
-files = index.get_files("CloudSat_2b_GeoProf",
-                        start = start,
-                        end = end)
-ind=0;
-file = files[ind].open()
-
-z = file.altitude
-x = file.latitude
-x = np.broadcast_to(x.reshape(-1, 1), z.shape)
-rr = file.radar_reflectivity
+    z = file.altitude
+    x = file.latitude
+    x = np.broadcast_to(x.reshape(-1, 1), z.shape)
+    rr = file.radar_reflectivity
 
 
-rr_data_element=np.ones([(int)(len(z)/64),64,64])*(-40)
-mask = rr_data_element == -8888
-rr_data_element =ma.masked_array(rr_data_element,mask=mask)
-# should we change the mask?
-for i in range(0, len(z)-64,64):
+    rr_data_element=np.ones([(int)(len(z)/64),64,64])*(-40)
+    mask = rr_data_element == -8888
+    rr_data_element =ma.masked_array(rr_data_element,mask=mask, dtype=np.float32)
+    # should we change the mask?
+    for i in range(0, len(z)-64,64):
 
-    for k in range(0,64):
-        j = 124
-        while z[i+k][j]<500:
-            j=j-1
+        for k in range(0,64):
+            j = 124
+            while z[i+k][j]<500:
+                j=j-1
 
-        n=j-64
-        test=(int) (i/64)
-        rr_data_element[test][k]=rr[i+k][j:n:-1]
+            n=j-64
+            test=(int) (i/64)
+            rr_data_element[test][k]=rr[i+k][j:n:-1]
 
-            #print(rr[i+k][l])
-print(type(rr_data_element))
-print(type(rr))
-xplot=range(0,64)
-yplot=range(0,64)
-from matplotlib.colors import Normalize
-f,ax = plt.subplots(1, 1)
-norm = Normalize(-30, 30)
-pcm = ax.pcolormesh(xplot, yplot , np.transpose(rr_data_element[250]), norm=norm)
+                #print(rr[i+k][l])
+    #print(type(rr_data_element))
+    #print(type(rr))
+    #xplot=range(0,64)
+    #yplot=range(0,64)
+    #from matplotlib.colors import Normalize
+    #f,ax = plt.subplots(1, 1)
+    #norm = Normalize(-30, 30)
+    #pcm = ax.pcolormesh(xplot, yplot , np.transpose(rr_data_element[250]), norm=norm)
 
-plt.show()
+    #plt.show()
+    return rr_data_element
--- network/test.py
+++ network/test.py
@@ -1,9 +1,14 @@
+
+import os
+import numpy.ma as ma
+from datetime import datetime
 import numpy as np
 import torch
 
-
+import matplotlib.pyplot as plt
 from GAN_generator import GAN_generator
 from GAN_discriminator import GAN_discriminator
+from Create_Dataset import create_dataset
 # parameters for the generator
 H_gen=[16384, 256, 128, 64, 1]
 D_in=384
@@ -13,6 +18,9 @@ model = GAN_generator(D_in,H_gen,D_out)
 x=torch.randn(D_in)
 #generated scene
 scene_gen=model(x)
+cloudsat_scenes= create_dataset()
+scene_real = torch.from_numpy(cloudsat_scenes[110])
+scene_real = scene_real.view(1,1,64,64)
 
 H_disc=[256, 128, 128, 5, 6, 64, 128, 256, 256, 4096, 1]
 D_in=[1, 5, 1,64]
@@ -22,8 +30,19 @@ scene=torch.randn([1,1,64,64])
 model = GAN_discriminator(D_in,H_disc,D_out)
 x=torch.randn(D_in)
 #probability that scene_gen is fake
-prob=model(x,scene_gen)
+prob=model(x,scene_real)
 print(prob)
+# script to plot the generated cloudsatimage
+# xplot=range(0,64)
+# yplot=range(0,64)
+# from matplotlib.colors import Normalize
+# f,ax = plt.subplots(1, 1)
+# norm = Normalize(-1, 1)
+# scene_gen_data = scene_gen.data.numpy()
+# pcm = ax.pcolormesh(xplot, yplot , (scene_gen_data[0][0]), norm=norm)
+#
+# plt.show()
+
 # print(len(y_pred[0]))
 # print(len(y_pred[0][0]))
 # print(len(y_pred[0][0][0]))
