ef1aefc0e2b85fe3e5395f264f75948f7f1ec631
rilemark <63635565+rilemark@users.noreply.github.com>
2020-09-16 11:48:35 +0200

    Uppdated plot examples and IWP method for CGAN

--- network/IceWaterPathMethod.py
+++ network/IceWaterPathMethod.py
@@ -8,7 +8,7 @@ from scipy import integrate
 
 
 
-def IceWaterPathMethod(cloudsat_scenes):
+def IceWaterPathMethod(cloudsat_scenes, zero_indices):
     scene_number = 0
     Z_m95 = np.power(10, cloudsat_scenes * 0.1)
     # Calculate IWC in g/m^3
@@ -16,7 +16,9 @@ def IceWaterPathMethod(cloudsat_scenes):
     IWC_ver2 = 0.198 * np.power(Z_m95, 0.701)
     IWC_ver3 = 0.108 * np.power(Z_m95, 0.770)
     print('IWC shape ', IWC.shape)
-
+    IWC = IWC.reshape(-1,1,64,64)
+    IWC_ver2 = IWC_ver2.reshape(-1,1,64,64)
+    IWC_ver3 = IWC_ver3.reshape(-1,1,64,64)
     # print('cloudsat ',example_scene[0])
     # print('IWC ',example_scene_IWC[0])
 
@@ -24,11 +26,28 @@ def IceWaterPathMethod(cloudsat_scenes):
     # IWC_vertical_integrals_ver2 = np.multiply(example_scene_IWC_ver2,240)
     # IWC_vertical_integrals_ver3 = np.multiply(example_scene_IWC_ver3,240)
 
-    IWC_vertical_integrals = np.multiply(IWC, 240)
-    IWC_vertical_integrals_ver2 = np.multiply(IWC_ver2, 240)
-    IWC_vertical_integrals_ver3 = np.multiply(IWC_ver3, 240)
+    # Calculate altitude for freeze levelzero_index_at_position:
+    IWC_above_freeze_1 = np.zeros([len(IWC),1,64,64])
+    IWC_above_freeze_2 = np.zeros([len(IWC_ver2), 1, 64, 64])
+    IWC_above_freeze_3 = np.zeros([len(IWC_ver3), 1, 64, 64])
+
+    for scene in range(0,len(zero_indices)):
+        for position in range(0,len(zero_indices[0,0])):
+            zero_index_at_position = int(zero_indices[scene,0,position])
+            IWC_above_freeze_1[scene,0,position,zero_index_at_position:] = IWC[scene,0,position,zero_index_at_position:]
+            IWC_above_freeze_2[scene, 0, position,zero_index_at_position:] = IWC_ver2[scene, 0, position, zero_index_at_position:]
+            IWC_above_freeze_3[scene, 0, position,zero_index_at_position:] = IWC_ver3[scene, 0, position, zero_index_at_position:]
+
+    IWC_vertical_integrals = np.multiply(IWC_above_freeze_1, 240)
+    IWC_vertical_integrals_ver2 = np.multiply(IWC_above_freeze_2, 240)
+    IWC_vertical_integrals_ver3 = np.multiply(IWC_above_freeze_3, 240)
+
+    IWC_vertical_integrals = torch.tensor(IWC_vertical_integrals)
+    IWC_vertical_integrals_ver2 = torch.tensor(IWC_vertical_integrals_ver2)
+    IWC_vertical_integrals_ver3 = torch.tensor(IWC_vertical_integrals_ver3)
+
     # Calculate IWP from IWC
-    IWC_vertical_sum = torch.sum(IWC_vertical_integrals, dim=3)  # dim=1 for one scene
+    IWC_vertical_sum = torch.sum(IWC_vertical_integrals, dim=3)  # dim=1 for one scene, dim =3 if [x,x,x,x]
     IWC_vertical_sum_ver2 = torch.sum(IWC_vertical_integrals_ver2, dim=3)
     IWC_vertical_sum_ver3 = torch.sum(IWC_vertical_integrals_ver3, dim=3)
     # Convert from
--- network/plot_examples_cgan.py
+++ network/plot_examples_cgan.py
@@ -7,7 +7,7 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 folder = './'
-checkpoint_parameter = torch.load(folder + 'network_parameters_CGAN.pt',map_location=torch.device('cpu'))
+checkpoint_parameter = torch.load(folder + 'network_parameters_CGAN_3500.pt',map_location=torch.device('cpu'))
 noise_parameter = checkpoint_parameter['noise_parameter']
 print(noise_parameter)
 H_gen=[576,16384, 256, 128, 64, 1]
@@ -19,6 +19,7 @@ beta1 = 0.5
 criterion = torch.nn.BCELoss()
 lr = 0.0002
 
+f_size = 14
 
 netG = GAN_generator(H_gen).float().to(device)
 optimizerG = torch.optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
@@ -34,10 +35,8 @@ for cloudsat_file in range(0, 4900):
     file_string = location + 'rr_modis_cloudsat_data_2015_' + str(cloudsat_file).zfill(4) + '.h5'
     if path.exists(file_string):
         hf = h5py.File(file_string, 'r')
-
         cloudsat_scenes_temp = torch.tensor(hf.get('rr')).view(-1, 1, 64, 64)
         modis_scenes_temp = torch.tensor(hf.get('emissivity')).view(-1, 1, 64, 10).float()
-
         if counter == 0:
             cloudsat_scenes = cloudsat_scenes_temp
             modis_scenes = modis_scenes_temp
@@ -45,8 +44,6 @@ for cloudsat_file in range(0, 4900):
         else:
             cloudsat_scenes = torch.cat([cloudsat_scenes, cloudsat_scenes_temp], 0)
             modis_scenes = torch.cat([modis_scenes, modis_scenes_temp], 0)
-
-
 temp_modis_scenes = torch.cat([modis_scenes[:,:,:,0:3],modis_scenes[:,:,:,4:9]],3)
 modis_scenes=temp_modis_scenes
 '''
@@ -64,8 +61,9 @@ def t_count(t, val):
 
 
 
-location = './modis_cloudsat_data/'
-file_string = location + 'modis_cloudsat_test_data_conc_ver2' + '.h5'
+location = './'
+file_string = location + 'CGAN_test_data_with_temp_conc_ver2.h5' #for CGAN 2015
+#file_string = location + 'modis_cloudsat_ElevLatLong_test_data_conc_2016_ver2.h5' #for CGAN 2016
 hf = h5py.File(file_string, 'r')
 
 cloudsat_scenes = torch.tensor(hf.get('cloudsat_scenes'))
@@ -75,12 +73,10 @@ print('modis scenes input size: ', modis_scenes.shape)
 temp_modis_scenes = torch.cat([modis_scenes[:, :, :, 0:3], modis_scenes[:, :, :, 4:9]], 3)
 modis_scenes = temp_modis_scenes
 print('modis after change size: ', modis_scenes.shape)
-dataset = torch.utils.data.TensorDataset(cloudsat_scenes, modis_scenes)
+temperature = torch.tensor(hf.get('temperature'))
+dataset = torch.utils.data.TensorDataset(cloudsat_scenes, modis_scenes,temperature)
 
-print('nuber of missing values in modis ', t_count(modis_scenes,(float(-1))))
-
-
-
+print('number of missing values in modis ', t_count(modis_scenes,(float(-1))))
 
 
 
@@ -89,11 +85,10 @@ yplot=np.linspace(1,16.36,64)
 from matplotlib.colors import Normalize
 
 
-
 #f2, axtest = plt.subplots(1,1)
 #ax=axs[range(0,3),range(0,3)]
 norm = Normalize(-35, 20)
-f, axs = plt.subplots(7,5, figsize=(13,15))
+f, axs = plt.subplots(8,5, figsize=(13,18))
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True,
                                              num_workers=2)
 file_number = 0
@@ -106,102 +101,158 @@ for i, data in enumerate(dataloader,0):
     print('cs first element: ',cs[0,0,0,0])
     string_ex = 'example' + str(i)
     modis = data[1]
+    temperature_data = data[2]
+    print(temperature_data.shape, 'shape of temperature data')
 
     modis = torch.transpose(modis, 1, 3)
     modis = torch.transpose(modis, 2, 3)
-    print(modis.shape)
+    print('modis shape', modis.shape)
     modis = modis.to(device)
-    pl = axs[1,i].plot(xplot,np.transpose(np.array(modis[0,:,0,:])))
-    axs[1,i].set_ylim([-1,1])
-    axs[1,i].minorticks_on()
-    axs[1, i].tick_params(axis='both', which='major', labelsize='12')
-    axs[1, i].tick_params(axis='both', which='minor')
-    axs[1,i].tick_params(labelbottom=False)
+    pl = axs[2,i].plot(xplot,np.transpose(np.array(modis[0,:,0,:])))
+    axs[2,i].set_ylim([-1,1])
+    axs[2,i].minorticks_on()
+    axs[2, i].tick_params(axis='both', which='major', labelsize=f_size)
+    axs[2, i].tick_params(axis='both', which='minor')
+    axs[2,i].tick_params(labelbottom=False)
     if i == 0:
-        axs[1, i].set_ylabel('Modis Input', fontsize=12)
+        axs[2, i].set_ylabel('Modis Input', fontsize=f_size)
     else:
-        axs[1, i].tick_params(labelleft=False)
+        axs[2, i].tick_params(labelleft=False)
 
     cs = (cs + 1) * (55 / 2) - 35
-    [IWP_cs_1, IWP_cs_2, IWP_cs_3] = IceWaterPathMethod((cs))
+
+    # Calculate cloud top height:
+    altitudes_cs = np.zeros([1, 64])
+    for k in range(0, 64):
+        first_cloud_location_cs = 0
+        for j in range(63, -1, -1):  # Loop backwards, starting from top of scene
+            if cs[0,0, k, j] >= -15 and first_cloud_location_cs == 0:  # Set dBZ limit for cloud top detection
+                altitudes_cs[0, k] = j  # save the index of the altitude where the cloud top is for each position
+                first_cloud_location_cs = +1
+    altitudes_cs = (altitudes_cs * 0.24) + 1  # Altitude of cloud top over sea level, [km]
+    #print('Altitudes of cloud tops (CloudSat):', altitudes_template, ' [km]')
+
+    # Calculate indices for IWP
+    indexes_zero = np.zeros([len(temperature_data), 1, 64, 1])
+
+    for l in range(0, len(temperature_data)):
+        for position in range(0, 64):
+            for j in range(len(temperature_data[l, 0, position]) - 2, -1, -1):
+                if (temperature_data[l, 0, position, j] - 273.15) * (temperature_data[l, 0, position, j + 1] - 273.15) <= 0:
+                    indexes_zero[l, 0, position] = j + 1
+                    break
+                if j == 0:
+                    if torch.max(temperature_data[l, 0, position, :]) <= 273.15:
+                        indexes_zero[l, 0, position] = 63
+                    else:
+                        indexes_zero[l] = 0
+                        print('All above zero: ', temperature_data[l, 0, position])
+
+    print(indexes_zero.shape, ' shape of indices zero')
+    indexes_zero_for_cs = np.ones([len(indexes_zero), 1, 64, 1]) * 63 - indexes_zero
+
+    [IWP_cs_1, IWP_cs_2, IWP_cs_3] = IceWaterPathMethod(cs,indexes_zero_for_cs)
     cs = cs.to(device)
-    pcm1 = axs[6, i].pcolormesh(xplot, yplot, np.transpose(cs.cpu().detach().numpy()[0,0]), norm=norm)
+    print('cloudsat shape ', cs.shape)
+    pcm1 = axs[7, i].pcolormesh(xplot, yplot, np.transpose(cs.cpu().detach().numpy()[0,0]), norm=norm)
     title_str = 'Scene' + str(i)
-    #axs[6, i].set_title(title_str, fontsize=2)
+    #axs[7, i].set_title(title_str, fontsize=2)
     #cb = f.colorbar(pcm, ax=axs[counter, 0])
     #cb.set_label('Reflectivities [dBZ]', fontsize=2)
-    axs[6, i].minorticks_on()
-    axs[6,i].set_xlabel('Position [km]', fontsize=12)
+    axs[7, i].minorticks_on()
+    axs[7,i].set_xlabel('Position [km]', fontsize=f_size)
     if i == 0:
-        axs[6, i].set_ylabel('Real \nAltitude [km]', fontsize=12)
+        axs[7, i].set_ylabel('Real \nAltitude [km]', fontsize=f_size)
     else:
-        axs[6, i].tick_params(labelleft=False)
-    axs[6, i].tick_params(axis='both', which='both', labelsize='12')
+        axs[7, i].tick_params(labelleft=False)
+    axs[7, i].tick_params(axis='both', which='both', labelsize=f_size)
     #axs[6, i].set_aspect(1.5)
     #cb.ax.tick_params(labelsize=2)
+
     color_array = ['blue','green','red','purple']
     for j in range(0,4):
         noise = (torch.randn(D_in_gen)).to(device)
         output = netG(noise, modis)
         output = (output + 1) * (55 / 2) - 35
 
-        pcm = axs[6-(j+1), i].pcolormesh(xplot, yplot, output.detach().numpy()[0,0], norm=norm)
+        pcm = axs[7-(j+1), i].pcolormesh(xplot, yplot, output.detach().numpy()[0,0], norm=norm)
         title_str = 'Scene' + str(i)
         #axs[6-(j+1), i].set_title(title_str, fontsize=2)
         #cb = f.colorbar(pcm, ax=axs[counter, 0])
         #cb.set_label('Reflectivities [dBZ]', fontsize=2)
-        axs[6-(j+1), i].minorticks_on()
-        axs[6-(j+1), i].tick_params(axis='both', which='major', labelsize='12')
-        axs[6-(j+1), i].tick_params(axis='both', which='minor')
-        axs[6-(j+1),i].tick_params(labelbottom=False)
+        axs[7-(j+1), i].minorticks_on()
+        axs[7-(j+1), i].tick_params(axis='both', which='major', labelsize=f_size)
+        axs[7-(j+1), i].tick_params(axis='both', which='minor')
+        axs[7-(j+1),i].tick_params(labelbottom=False)
         if i == 0:
-            axs[6-(j+1), i].set_ylabel('Generated \nAltitude [km]', fontsize=12)
+            axs[7-(j+1), i].set_ylabel('Generated \nAltitude [km]', fontsize=f_size)
         else:
-            axs[6 - (j + 1), i].tick_params(labelleft=False)
+            axs[7 - (j + 1), i].tick_params(labelleft=False)
 
         with torch.no_grad():
-            [IWP_generated_1, IWP_generated_2, IWP_generated_3] = IceWaterPathMethod(torch.transpose(output, 2, 3))
+
+            [IWP_generated_1, IWP_generated_2, IWP_generated_3] = IceWaterPathMethod(torch.transpose(output, 2, 3),indexes_zero_for_cs)
         pcm = axs[0, i].semilogy(xplot, (IWP_generated_1[0, 0]), color=color_array[j], label='IWP generated', basey=10, alpha=0.5)
         #axs[6-(j+1), i].set_aspect(1.5)
         #cb.ax.tick_params(labelsize=2)
+        print('generated scenes shape: ',output.shape)
+        # Calculate cloud top height:
+        altitudes_generated = np.zeros([1, 64])
+        for k in range(0, 64):
+            first_cloud_location_gen = 0
+            for l in range(63, -1, -1):  # Loop backwards, starting from top of scene
+                if output[0,0,l,k] >= -15 and first_cloud_location_gen == 0:  # Set dBZ limit for cloud top detection
+                    altitudes_generated[0, k] = l  # save the index of the altitude where the cloud top is for each position
+                    first_cloud_location_gen = +1
+        altitudes_generated = (altitudes_generated * 0.24) + 1  # Altitude of cloud top over sea level, [km]
 
-
+        alt = axs[1,i].plot(xplot, np.transpose(altitudes_generated), color = color_array[j], label ='CTH generated',alpha=0.3)
+    alt_cs = axs[1,i].plot(xplot, np.transpose(altitudes_cs),color='black', label='CTH real')
+    axs[1, i].minorticks_on()
+    axs[1, i].tick_params(axis='both', which='major', labelsize=f_size)
+    axs[1, i].tick_params(axis='both', which='minor')
+    axs[1, i].tick_params(labelbottom=False)
+    if i == 0:
+        axs[1, i].set_ylabel('Cloud-Top \nHeight [km]', fontsize=f_size)
+    else:
+        axs[1, i].tick_params(labelleft=False)
     #output = output.cpu().detach().numpy()[0, 0]
-
+    axs[1, i].set_ylim(1, 16.36)
 
     axs[0, i].minorticks_on()
-    axs[0, i].tick_params(axis='both', which='major', labelsize='12')
+    axs[0, i].tick_params(axis='both', which='major', labelsize=f_size)
     axs[0, i].tick_params(axis='both', which='minor')
-    axs[0,i].set_ylim([2**(-4),2**4])
+    #axs[0,i].set_ylim([2**(-4),2**4])
+    axs[0, i].set_ylim([10 ** (-2), 10 ** 1])
 
     pcm = axs[0, i].semilogy(xplot, (IWP_cs_1[0,0]), color='black', label='IWP real',basey=10)
     title_str = 'IWP generated' + str(i)
     #axs[0,i].set_title(title_str, fontsize=2)
     axs[0, i].tick_params(labelbottom=False)
     if i == 0:
-        axs[0, i].set_ylabel('IWP [g m⁻²]', fontsize=12)
+        axs[0, i].set_ylabel('IWP [g m⁻²]', fontsize=f_size)
     else:
         axs[0, i].tick_params(labelleft=False)
     #axs[0, i].set_aspect(1/4)
 
 f.tight_layout()
 f.subplots_adjust(right=0.88)
-cbar_ax = f.add_axes([0.9, 0.045, 0.025, 0.665])
+cbar_ax = f.add_axes([0.9, 0.0375, 0.025, 0.59]) #([distance from left, bottom height, width, height])
 cbar1= f.colorbar(pcm1, cax=cbar_ax)
-cbar1.set_label('Reflectivities [dBZ]', fontsize=12)
-cbar1.ax.tick_params(labelsize=12)
+cbar1.set_label('Reflectivities [dBZ]', fontsize=f_size)
+cbar1.ax.tick_params(labelsize=f_size)
 
 
 
 '''
-
     pcm =axs[counter,2].plot(xplot, IWP_generated_1[0,0], label='IWP ver1')
     title_str = 'IWP generated' + str(i)
     axs[counter, 2].set_title(title_str, fontsize=2)
-
     pcm = axs[counter, 3].plot(xplot, IWP_cs_1[0,0], label='IWP ver1')
     title_str = 'IWP cloudsat' + str(i)
     axs[counter, 2].set_title(title_str, fontsize=2)
 '''
 print('image saved as: ', 'testepoch' + str(epoch) + '_CGAN_ver8_1')
-plt.savefig('testepoch' + str(epoch) + '_CGAN_ver8_1')
+#plt.savefig('testepoch' + str(epoch) + '_CGAN_ver8_1')
+plt.savefig('./Results/ExamplesCGAN/testepoch' + str(epoch) + '_CGAN_2015'+'_example1')
+
